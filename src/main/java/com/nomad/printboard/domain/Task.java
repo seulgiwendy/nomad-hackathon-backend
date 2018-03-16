7657138f5dd8abd61093cfdef5df19af38e52856
seulgiwendy <wheejuni@gmail.com>
2018-03-16 23:03:20 +0900

    [project]Jenkins JAR 파일 실험

--- src/main/java/com/nomad/printboard/domain/Task.java
+++ src/main/java/com/nomad/printboard/domain/Task.java
@@ -1,7 +1,15 @@
 package com.nomad.printboard.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-public class Task {
+public class Task{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "TASK_ID")
+    private long id;
 }
