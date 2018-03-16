e2b5570997f273e2d895d7b32dfe9c85f0ce7d1e
seulgiwendy <wheejuni@gmail.com>
2018-03-16 21:29:20 +0900

    [project]Jenkins 연동 실험

--- src/main/java/com/nomad/printboard/PrintboardApplication.java
+++ src/main/java/com/nomad/printboard/PrintboardApplication.java
@@ -1,7 +1,9 @@
 package com.nomad.printboard;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrintboardApplication {
@@ -9,4 +11,11 @@ public class PrintboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrintboardApplication.class, args);
 	}
+
+	@Bean
+    CommandLineRunner commandLineRunner() {
+	    return args -> {
+	      System.out.println("something changed! please build me!");
+        };
+    }
 }
