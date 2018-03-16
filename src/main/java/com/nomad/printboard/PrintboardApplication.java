cb9d8c26736d96c5a22a7bf82532b515f2d6e9de
seulgiwendy <wheejuni@gmail.com>
2018-03-16 21:26:05 +0900

    initial commit

---
+++ src/main/java/com/nomad/printboard/PrintboardApplication.java
@@ -0,0 +1,12 @@
+package com.nomad.printboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintboardApplication.class, args);
+	}
+}
