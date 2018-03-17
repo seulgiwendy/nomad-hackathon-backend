2a634d0e2a5f476bd89915bfc4e77dca7b05b981
seulgiwendy <wheejuni@gmail.com>
2018-03-17 11:30:56 +0900

    [project]Profile 추가, 확인 컨트롤러 추가

---
+++ src/main/java/com/nomad/printboard/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.nomad.printboard.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+                .authorizeRequests()
+                .antMatchers("/profile")
+                .permitAll();
+    }
+}
