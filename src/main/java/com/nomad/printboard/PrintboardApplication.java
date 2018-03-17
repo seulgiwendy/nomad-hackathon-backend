6ab0c0ab69babd3d5b2ddfcc8ffc8c0ec2d823b0
seulgiwendy <wheejuni@gmail.com>
2018-03-17 11:54:49 +0900

    [project]Profiles 외재화

--- src/main/java/com/nomad/printboard/PrintboardApplication.java
+++ src/main/java/com/nomad/printboard/PrintboardApplication.java
@@ -3,13 +3,20 @@ package com.nomad.printboard;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrintboardApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/home/ubuntu/app/config/printboard/prod-config.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(PrintboardApplication.class, args);
+		new SpringApplicationBuilder(PrintboardApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
 	}
 
 
